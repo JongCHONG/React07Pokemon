ff4438de9cbe16f3b7ccb2752610e77dd3bcd56a
unknown <jochong27@gmail.com>
2021-11-17 20:19:55 +0100

    Création de la page Home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,6 +10,9 @@ const Home = () => {
         <li className="nav-item">
           <Link className="nav-link" to="/login">Login</Link>
         </li>
+        <li className="nav-item">
+          <button type="button" className="btn btn-outline-primary">Random</button>
+        </li>
       </ul>
     </>
   )
