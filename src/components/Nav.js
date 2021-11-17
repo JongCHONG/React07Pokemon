9347533e9bfd38c5a6268a44fd6096af0f076204
unknown <jochong27@gmail.com>
2021-11-17 20:37:38 +0100

    Random

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,9 +10,6 @@ const Home = () => {
         <li className="nav-item">
           <Link className="nav-link" to="/login">Login</Link>
         </li>
-        <li className="nav-item">
-          <button type="button" className="btn btn-outline-primary">Random</button>
-        </li>
       </ul>
     </>
   )
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,19 @@ const Home = () => {
       .then(result => { setPokemon(result) }) // on détaille l'action à exécuter sur ce JSON
   }, [])
 
+  const Random = () => {
+    const min = 1
+    const max = 151
+    const pokemonID = Math.floor(Math.random() * (max - min + 1) + min) // donnera un nombre aléatoire entre 1 et 10
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonID}`)
+      .then(response => response.json()) // on transforme la donnée reçue en JSON 
+      .then(result => { setPokemon(result) }) // on détaille l'action à exécuter sur ce JSON
+  }
   // console.log(types)
   return(
     <>
         <h1>Home</h1>
+        <button type="button" className="btn btn-outline-primary" onClick={() => Random()}>Random</button>
         {pokemon === null ? "" : 
           <>
             <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
